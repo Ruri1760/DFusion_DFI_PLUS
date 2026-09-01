292666e39edeb15c08f90a286f99d1964721fa3d
Ruri1760 <2107162581@qq.com>
2026-09-01 23:07:42 +0800

    Remove cross_attention class from ID_Fusion.py
    Removed cross_attention class and its methods from ID_Fusion.py.

--- ArcDFI/utils/ID_Fusion.py
+++ ArcDFI/utils/ID_Fusion.py
@@ -3,29 +3,6 @@ import torch.nn as nn
 from torch.nn import Parameter
 import torch.nn.functional as F
 
-'''
-
-class cross_attention(nn.Module):
-    def __init__(self, hidden1, hidden2):
-        super(cross_attention, self).__init__()
-        self.W_q = nn.Linear(hidden1, hidden2)
-        self.W_k = nn.Linear(hidden2, hidden2)
-        self.W_v = nn.Linear(hidden2, hidden2)
-        self.drop = nn.Dropout(p=0.1)
-        self.gelu = nn.GELU()
-        self.softmax = nn.Softmax(-1)
-
-    def forward(self, xs, x): # xs, x两个模态的表示，先做交叠注意力，然后再拼接
-        q = self.W_q(x)
-        k = self.W_k(xs)
-        v = self.W_v(xs)
-        weight = torch.matmul(q, k.permute(0, 2, 1))
-        scale = weight.size(-1) ** -0.5
-        weights = self.softmax(weight * scale)
-        ys = torch.matmul(self.drop(weights), v) + xs
-        return ys
-
-'''
 class ID_Fusion(nn.Module):
     def __init__(self, hidden1, hidden2):
         super(ID_Fusion, self).__init__()
@@ -36,7 +13,7 @@ class ID_Fusion(nn.Module):
         self.gelu = nn.GELU()
         self.softmax = nn.Softmax(-1)
 
-    def forward(self, x, y, z): # xs, x两个模态的表示，先做交叠注意力，然后再拼接
+    def forward(self, x, y, z): 
         q = self.W_q(x)
         k = self.W_k(y)
         v = self.W_v(z)
